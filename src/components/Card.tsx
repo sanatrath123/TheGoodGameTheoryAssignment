ecbeee9ff4945ce2faf04c6fd08454023deeda13
sanatrath123 <rathsanantakumar@gmail.com>
2024-08-01 15:39:20 +0530

    handle overflow text

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,14 +1,23 @@
 import { motion } from "framer-motion";
+import { useState } from "react";
+
 
 type CardProps = {
   quote: string;
 };
 
 const Card = ({ quote }: CardProps) => {
-    console.log("card rander" ,quote)
+
+  const [ expand , setExpand ] = useState<boolean>(false)
+  const textover = quote.length > 50 ? quote.slice(0, 50) + '...' : quote;
+  
+const Showmore = ()=>{
+  setExpand(!expand)
+}
+
   return (
     <motion.div
-      className="quote-card w-80 rounded-none bg-sky-400 text-gray-100 m-auto min-h-96 font-semibold flex px-3 relative overflow-hidden"
+      className={`quote-card w-80 rounded-none bg-sky-400 text-gray-100 m-auto min-h-[24rem] font-semibold flex justify-center items-center px-3 relative overflow-hidden  ${expand ? 'h-[35rem] w-96' : 'h-[24rem]'} `}
       animate={{
         scale: [1, 2, 2, 1, 1],
         rotate: [0, 0, 270, 270, 0],
@@ -20,14 +29,30 @@ const Card = ({ quote }: CardProps) => {
         times: [0, 0.2, 0.5, 0.8, 1],
       }}
     >
-      <img src="public/Logo_of_Twitter.svg"  className="h-16 w-16 rounded-full absolute left-[50%] top-5 transform -translate-x-1/2  transition-transform duration-500 ease-in-out" alt="" />
-      <p className="m-auto text-2xl w-full text-center text-wrap">{quote}</p>
+      <img src="/Logo_of_Twitter.svg"  className="h-16 w-16 rounded-full absolute left-[50%] top-3 transform -translate-x-1/2  transition-transform duration-500 ease-in-out" alt="" />
 
-      <span className="text-gray-100 text-md font-thin absolute bottom-[4.2rem] left-2">Aug 1 2024</span>
 
-      <div className="absolute bottom-0 left-0 h-16 border-t-2 border-gray-100 w-full text-2xl font-semibold text-gray-800 flex justify-between px-3 items-center">
+
+      <p className="m-auto text-2xl w-full text-center text-wrap">{
+        expand ? quote : textover
+
+        }
+      
+      {/* this is only when text is more than 150 char */}
+      {/* showmore and showless logic */}
+{ quote.length >150 && <span
+onClick={Showmore}
+className="text-gray-900 text-lg cursor-pointer border-b-2 border-gray-100">{expand ? "Show less":"Show more"}</span>}
+      </p>
+   
+
+      <span className="text-gray-100 text-md font-thin absolute bottom-[4rem] left-2">Aug 1 2024</span>
+
+      <div className="absolute bottom-0 left-0 h-[3.5rem] border-t-2 border-gray-100 w-full text-2xl font-semibold text-gray-800 flex justify-between px-3 items-center">
         <p className=" text-gray-900 text-xl font-medium">GitHub:</p>
-<p className="cursor-pointer">@sanatrath123</p>
+        <a href="https://github.com/sanatrath123" target="_blank" rel="noopener noreferrer">
+    <p className="cursor-pointer">@sanatrath123</p>
+  </a>
       </div>
     </motion.div>
   );
